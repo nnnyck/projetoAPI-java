5b6861349df59f0b1972409736c3dd12adb74933
Nyckolas <111590250+nnnyck@users.noreply.github.com>
2023-12-06 19:11:19 -0300

    Update UsuarioDto.java

--- src/main/java/br/com/criandoapi/projeto/dto/UsuarioDto.java
+++ src/main/java/br/com/criandoapi/projeto/dto/UsuarioDto.java
@@ -1,19 +1,33 @@
-package br.com.criandoapi.projeto.dto;
+package br.com.criandoapi.projeto.service;
 
-import lombok.Getter;
-import lombok.Setter;
+import br.com.criandoapi.projeto.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Getter
-@Setter
-public class UsuarioDto {
-	private String nome;
-	private String email;
-	private String senha;
-	
-	public UsuarioDto(String nome, String email, String senha) {
-		super();
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-	}
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public void sincronizarComBancoDeDados(List<Usuario> usuariosJava) {
+        List<Usuario> usuariosBancoDeDados = entityManager.createQuery("SELECT u FROM Usuario u", Usuario.class)
+                .getResultList();
+
+        // Identificar usuários para remover
+        List<Usuario> usuariosParaRemover = usuariosBancoDeDados.stream()
+                .filter(usuario -> !usuariosJava.contains(usuario))
+                .collect(Collectors.toList());
+
+        // Remover usuários inexistentes no Java, mas existentes no banco de dados
+        for (Usuario usuario : usuariosParaRemover) {
+            entityManager.remove(usuario);
+        }
+    }
 }
